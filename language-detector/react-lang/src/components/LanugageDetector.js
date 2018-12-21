afe6c47e5b2543ac8c7ba1fb1e921120b0630807
Eric Groom <egroom@mail.sfsu.edu>
2018-12-20 19:11:17 -0800

    language detector implementation

---
+++ language-detector/react-lang/src/components/LanugageDetector.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./LanguageDetector.css";
+
+class LanguageDetector extends React.Component {
+  state = {
+    loading: false,
+    text: "",
+    language: null
+  };
+  abortController = new AbortController();
+  lastRequest = new Date();
+  getLanguage = async () => {
+    // Space out api requests
+    const now = new Date();
+    if (now.getTime() - this.lastRequest.getTime() < 1000) return;
+    this.lastRequest = new Date();
+    // abort any current requests
+    this.abortController.abort();
+    this.setState({ loading: true });
+    try {
+      const res = await fetch(
+        `http://apilayer.net/api/detect?access_key=${
+          process.env.REACT_APP_LANG_API_KEY
+        }&query=${this.state.text}`,
+        { abort: this.abortController }
+      );
+      const data = await res.json();
+      this.setState({ language: data.results[0].language_name });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  handleChange = async e => {
+    this.setState({ text: e.target.value });
+    await this.getLanguage();
+  };
+  render() {
+    return (
+      <div>
+        <p>I know what you speak</p>
+        <input
+          type="text"
+          placeholder="type some text..."
+          value={this.state.text}
+          onChange={this.handleChange}
+        />
+        <p>Language: {this.state.language}</p>
+      </div>
+    );
+  }
+}
+
+export default LanguageDetector;
