c772bd2281d059259c919b08ce887c157d4b3c8f
Eric Groom <egroom@mail.sfsu.edu>
2018-12-20 18:07:29 -0800

    angular performance implementation

--- performance-testing/angular-test/src/app/performance-evaluation/performance-evaluation.component.ts
+++ performance-testing/angular-test/src/app/performance-evaluation/performance-evaluation.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+interface Item {
+  key: number;
+  value: number;
+}
 
 @Component({
-  selector: 'app-performance-evaluation',
-  templateUrl: './performance-evaluation.component.html',
-  styleUrls: ['./performance-evaluation.component.css']
+  selector: "app-performance-evaluation",
+  templateUrl: "./performance-evaluation.component.html",
+  styleUrls: ["./performance-evaluation.component.css"]
 })
 export class PerformanceEvaluationComponent implements OnInit {
+  items: Item[] = [];
+  index: number = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  addRandom(quantity: number): void {
+    const start = new Date();
+    for (let i = 0; i < quantity; i++) {
+      const value = Math.random();
+      const key = this.index + 1;
+      this.items = [...this.items, { key, value }];
+      this.index = key;
+    }
+    const end = new Date();
+    console.log("time to add:", end.getTime() - start.getTime());
   }
 
+  sort(): void {
+    const start = new Date();
+    const sorted = this.items.slice().sort((a, b) => a.value - b.value);
+    this.items = sorted;
+    const end = new Date();
+    console.log("time to sort:", end.getTime() - start.getTime());
+  }
 }
